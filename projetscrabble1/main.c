d47b07179201def289d371600aedea043e2df043
Inès K <61450751+inesked@users.noreply.github.com>
2020-10-20 17:00:23 +0200

    menu

--- projetscrabble1/main.c
+++ projetscrabble1/main.c
@@ -3,8 +3,34 @@
 
 int main()
 {
-    printf("Hello Ines \n ca va toi ?!\n");
-    return 0;
-    //test
+    int choix;
 
+    printf("\n******************SCRABBLE******************\n\n");
+    printf("------Veuillez selectioner une action------ \n1. Pour lancer une nouvelle partie \n2. Pour reprendre une partie sauvegardée\n");
+    printf("3. Pour afficher l'aide \n4. Pour afficher les scores des joueurs \n5. Pour quitter le jeu\n");
+    scanf("%d", &choix);
+
+    switch (choix)
+    {
+    case 1:
+
+        break;
+
+    case 2:
+        break;
+
+    case 3:
+        break;
+
+    case 4:
+        break;
+
+    case 5:
+        printf("a bientot ! :)");
+        return 0;
+        break;
+
+
+        return 0;
+    }
 }
